6ec1afff60bdf793752fb84c11b0e37d93aaf94e
David Grande <dbgrande246@gmail.com>
2022-05-09 08:43:43 -0700

    Add track and hold to LFO4.

--- src/LFO4.cpp
+++ src/LFO4.cpp
@@ -56,6 +56,7 @@ struct LFO4 : Module {
 		FREQB_PARAM,
 		FREQC_PARAM,
 		FREQD_PARAM,
+		NEG_MODE_PARAM,
 		NUM_PARAMS
 	};
 	enum InputIds {
@@ -74,6 +75,7 @@ struct LFO4 : Module {
 		B_LIGHT,
 		C_LIGHT,
 		D_LIGHT,
+		NEG_MODE_LIGHT,
 		NUM_LIGHTS
 	};
 
@@ -86,6 +88,7 @@ struct LFO4 : Module {
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
 		configParam(FM_PARAM, -6.f, 8.f, 1.f, "Frequency", "Hz", 2, 1);
 		configInput(FM_INPUT, "Frequency CV");
+		configButton(NEG_MODE_PARAM, "Negative CV mode");
 		configParam(FREQA_PARAM, -8.f, 8.f, 0.f, "A scaling ratio", "×", 2, 1);
 		configOutput(SINA_OUTPUT, "Sine A");
 		configParam(FREQB_PARAM, -8.f, 8.f, 0.f, "B scaling ratio", "×", 2, 1);
@@ -96,6 +99,11 @@ struct LFO4 : Module {
 		configOutput(SIND_OUTPUT, "Sine D");
 	}
 
+	int last_mode = -1;
+	int mode = -1;
+	bool negative = false;
+	bool hold = false;
+
 	void process(const ProcessArgs& args) override {
 		float fmParam = params[FM_PARAM].getValue();
 		float freqAParam = params[FREQA_PARAM].getValue();
@@ -103,15 +111,31 @@ struct LFO4 : Module {
 		float freqCParam = params[FREQC_PARAM].getValue();
 		float freqDParam = params[FREQD_PARAM].getValue();
 
-		int channelsA = std::max(1, inputs[FM_INPUT].getChannels());
-		int channelsB = channelsA;
-		int channelsC = channelsA;
-		int channelsD = channelsA;
+		int channels = std::max(1, inputs[FM_INPUT].getChannels());
 
-		for (int c = 0; c < channelsA; c += 4) {
+		// Read button
+		mode = clamp((int)(params[NEG_MODE_PARAM].getValue()), 0, 1);
+		if (mode == 1) {
+			if (last_mode == 0) {
+				negative = !negative;
+				last_mode = 1;
+			}
+		}
+		else
+			last_mode = 0;
+		lights[NEG_MODE_LIGHT].setBrightness(negative);
+
+		// FM Input polarity value
+		float polarity = negative ? -1.f : 1.f;  // polarity of cv input
+		if (hold) {
+			polarity *= 5.f;  // scale cv input for sample and hold
+		}
+
+		// LFO A
+		for (int c = 0; c < channels; c += 4) {
 			auto* oscillatorA = &oscillatorsA[c / 4];
 
-			float_4 pitch = inputs[FM_INPUT].getVoltageSimd<float_4>(c) + fmParam;
+			float_4 pitch = inputs[FM_INPUT].getVoltageSimd<float_4>(c) * polarity + fmParam;
 			pitch += freqAParam;
 			oscillatorA->setPitch(pitch);
 
@@ -122,10 +146,11 @@ struct LFO4 : Module {
 				outputs[SINA_OUTPUT].setVoltageSimd(5.f * oscillatorA->sin(), c);
 		}
 
-		for (int c = 0; c < channelsB; c += 4) {
+		// LFO B
+		for (int c = 0; c < channels; c += 4) {
 			auto* oscillatorB = &oscillatorsB[c / 4];
 
-			float_4 pitch = inputs[FM_INPUT].getVoltageSimd<float_4>(c) + fmParam;
+			float_4 pitch = inputs[FM_INPUT].getVoltageSimd<float_4>(c) * polarity + fmParam;
 			pitch += freqBParam;
 			oscillatorB->setPitch(pitch);
 
@@ -136,10 +161,11 @@ struct LFO4 : Module {
 				outputs[SINB_OUTPUT].setVoltageSimd(5.f * oscillatorB->sin(), c);
 		}
 
-		for (int c = 0; c < channelsC; c += 4) {
+		// LFO C
+		for (int c = 0; c < channels; c += 4) {
 			auto* oscillatorC = &oscillatorsC[c / 4];
 
-			float_4 pitch = inputs[FM_INPUT].getVoltageSimd<float_4>(c) + fmParam;
+			float_4 pitch = inputs[FM_INPUT].getVoltageSimd<float_4>(c) * polarity + fmParam;
 			pitch += freqCParam;
 			oscillatorC->setPitch(pitch);
 
@@ -150,10 +176,11 @@ struct LFO4 : Module {
 				outputs[SINC_OUTPUT].setVoltageSimd(5.f * oscillatorC->sin(), c);
 		}
 
-		for (int c = 0; c < channelsD; c += 4) {
+		// LFO D
+		for (int c = 0; c < channels; c += 4) {
 			auto* oscillatorD = &oscillatorsD[c / 4];
 
-			float_4 pitch = inputs[FM_INPUT].getVoltageSimd<float_4>(c) + fmParam;
+			float_4 pitch = inputs[FM_INPUT].getVoltageSimd<float_4>(c) * polarity + fmParam;
 			pitch += freqDParam;
 			oscillatorD->setPitch(pitch);
 
@@ -164,10 +191,10 @@ struct LFO4 : Module {
 				outputs[SIND_OUTPUT].setVoltageSimd(5.f * oscillatorD->sin(), c);
 		}
 
-		outputs[SINA_OUTPUT].setChannels(channelsA);
-		outputs[SINB_OUTPUT].setChannels(channelsB);
-		outputs[SINC_OUTPUT].setChannels(channelsC);
-		outputs[SIND_OUTPUT].setChannels(channelsD);
+		outputs[SINA_OUTPUT].setChannels(channels);
+		outputs[SINB_OUTPUT].setChannels(channels);
+		outputs[SINC_OUTPUT].setChannels(channels);
+		outputs[SIND_OUTPUT].setChannels(channels);
 
 
 		// Warning lights - approximate point when LFOs reach their upper or lower limits
@@ -185,8 +212,36 @@ struct LFO4 : Module {
 			lights[D_LIGHT].setBrightness(false);
 		}
 	}
-};
 
+	void onReset() override {
+		negative = false;
+		hold = false;
+	}
+
+	json_t* dataToJson() override {
+		json_t* rootJ = json_object();
+
+		// negative cv input for sample and hold
+		json_object_set_new(rootJ, "negative", json_boolean(negative));
+
+		// scale cv input for sample and hold
+		json_object_set_new(rootJ, "hold", json_boolean(hold));
+
+		return rootJ;
+	}
+
+	void dataFromJson(json_t* rootJ) override {
+		// negative cv input for sample and hold
+		json_t* negativeJ = json_object_get(rootJ, "negative");
+		if (negativeJ)
+			negative = json_boolean_value(negativeJ);
+
+		// scale cv input for sample and hold
+		json_t* holdJ = json_object_get(rootJ, "hold");
+		if (holdJ)
+			hold = json_boolean_value(holdJ);
+	}
+};
 
 
 struct LFO4Widget : ModuleWidget {
@@ -199,6 +254,8 @@ struct LFO4Widget : ModuleWidget {
 
 		addParam(createParamCentered<RoundSmallBlackKnob>(mm2px(Vec(5.08, 18.25)), module, LFO4::FM_PARAM));
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(5.08, 27.00)), module, LFO4::FM_INPUT));
+		addChild(createLightCentered<SmallLightFlat<BlueLight>>(mm2px(Vec(8.00, 22.625)), module, LFO4::NEG_MODE_LIGHT));
+		addParam(createParam<SmallLEDButton>(mm2px(Vec(8.00-1.5, 22.625-1.5)), module, LFO4::NEG_MODE_PARAM));
 
 		addChild(createLightCentered<PetiteStealthLight<RedLight>>(mm2px(Vec(8.20, 38.00)), module, LFO4::A_LIGHT));
 		addParam(createParamCentered<Trimpot>(mm2px(Vec(5.08, 40.80)), module, LFO4::FREQA_PARAM));
@@ -215,6 +272,15 @@ struct LFO4Widget : ModuleWidget {
 		addChild(createLightCentered<PetiteStealthLight<RedLight>>(mm2px(Vec(8.20, 104.00)), module, LFO4::D_LIGHT));
 		addParam(createParamCentered<Trimpot>(mm2px(Vec(5.08, 106.80)), module, LFO4::FREQD_PARAM));
 		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(5.08, 115.30)), module, LFO4::SIND_OUTPUT));
+	}
+
+	void appendContextMenu(Menu* menu) override {
+		LFO4* module = dynamic_cast<LFO4*>(this->module);
+		assert(module);
+
+		menu->addChild(new MenuSeparator);
+
+		menu->addChild(createBoolPtrMenuItem("Extended Hold Range", "", &module->hold));
 	}
 };
 
